b6b12a54d59260064a4d14bde61f0d071b66cc32
Daniel Foster <danieljfoster87@gmail.com>
2018-02-25 12:02:08 -0500

    began javascript coding

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// set a target number variable
+var targetNumber = 66;
+
+// display the target number to the page
+$("#target-number").text("Target Number : " + targetNumber);
+
+// start the current score counter at zero
+var counter = 0;
+
+// make an array of the four crystal number options
+var numberOptions = [6, 3, 11, 4];
+
+for (var i = 0; i < numberOptions.length; i++) {
+  
+}
